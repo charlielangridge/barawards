997047b679b1a7853daed7378f73dda270ecf71e
Charlie Langridge <charlie@charlielangridge.com>
2024-06-12 11:02:41 +0100

    Add Favoicons

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,6 +14,14 @@
         <script src="https://cdn.usefathom.com/script.js" data-spa="auto" data-site="GGEHMEBE" defer></script>
         <!-- / Fathom -->
 
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#029fa2">
+        <meta name="msapplication-TileColor" content="#029fa2">
+        <meta name="theme-color" content="#029fa2">
+
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
