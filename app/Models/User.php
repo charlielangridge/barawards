dde5168220b4b4610497efac0684df596685a5ee
Charlie Langridge <charlie@charlielangridge.com>
2024-06-12 15:06:55 +0100

    Add Spatie permission handling to Filament admin
    This commit introduces Spatie permission handling to the Filament admin panel. It includes implementing permission-related functionalities such as creating and editing users, listing users and configuring permissions. Also, additional configurations and migrations are added to support the new permission system.

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,16 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -45,8 +48,16 @@ class User extends Authenticatable
         ];
     }
 
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+        return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
+    }
+
     public function nominations()
     {
         return $this->hasMany(Nomination::class);
     }
+
 }
